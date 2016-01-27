976e96657aa25a9e5b6b7c12a4264b2ef372b669
(_| |_) <kaeru@kaeru.kaeru>
2016-01-27 12:37:15 +0900

    list to 2d dict

--- utils.py
+++ utils.py
@@ -9,3 +9,16 @@ def conv_dict_to_odict(d, first_sort=sorted, second_sort=sorted):
         ordered[k] = OrderedDict(sorted_value)
 
     return(ordered)
+
+
+def conv_list_to_2d_dict(list_data, get_column_value, get_header_value):
+    dict_2d = dict()
+    for l in list_data:
+        column_value = get_column_value(l)
+        header_value = get_header_value(l)
+        if not dict_2d.get(column_value):
+            dict_2d[column_value] = dict()
+        if not dict_2d[column_value].get(header_value):
+            dict_2d[column_value][header_value] = dict()
+        dict_2d[column_value][header_value] = l
+    return dict_2d
